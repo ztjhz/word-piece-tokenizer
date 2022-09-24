efd183053264568bbbfe3a02ea42945a63b4956b
Jing Hua <tohjinghua123@gmail.com>
2022-09-25 01:36:19 +0800

    remove checks

--- tests/tokenizer_test.py
+++ tests/tokenizer_test.py
@@ -14,15 +14,10 @@ from src.word_piece_tokenizer.WordPieceTokenizer import WordPieceTokenizer
 class TestTokenizer(unittest.TestCase):
     performance = []
 
-    def setUp(self, compare_performance=True):
+    def setUp(self):
         self._bert_tokenizer = BertTokenizer.from_pretrained(
             'bert-base-uncased')
         self._my_tokenizer = WordPieceTokenizer()
-        self._compare_performance = compare_performance
-    
-    def tearDown(self):
-        if self._compare_performance:
-            print(f"[Average] This tokenizer is {sum(TestTokenizer.performance) / len(TestTokenizer.performance) * 100:.2f}% faster")
 
     def tokenize_with_both_tokenizer(self, s: str):
         print()
@@ -30,24 +25,27 @@ class TestTokenizer(unittest.TestCase):
 
         time_taken = []
 
-        if (self._compare_performance): start_time = timeit.default_timer()
+        # HuggingFace BERT Tokenizer
+        start_time = timeit.default_timer()
         batch = self._bert_tokenizer([s])
         lib_res = batch.input_ids[0]
-        if (self._compare_performance): time_taken.append(timeit.default_timer() - start_time)
-        if (self._compare_performance): start_time = timeit.default_timer()
+        time_taken.append(timeit.default_timer() - start_time)
+
+        # This Tokenizer
+        start_time = timeit.default_timer()
         my_res = self._my_tokenizer.tokenize(s)
-        if (self._compare_performance): time_taken.append(timeit.default_timer() - start_time)
+        time_taken.append(timeit.default_timer() - start_time)
 
         print(lib_res)
         print(my_res)
 
-        if (self._compare_performance):
-            print("\n Performance Results:")
-            print(f"BERT tokenizer: {time_taken[0]}")
-            print(f"This tokenizer: {time_taken[1]}")
-            performance = 1 - time_taken[1] / time_taken[0]
-            print(f"This tokenizer is {100 * performance:.2f}% faster")
-            TestTokenizer.performance.append(performance)
+        print("\n Performance Results:")
+        print(f"BERT tokenizer: {time_taken[0]}")
+        print(f"This tokenizer: {time_taken[1]}")
+        performance = 1 - time_taken[1] / time_taken[0]
+        TestTokenizer.performance.append(performance)
+        print(f"This tokenizer is {100 * performance:.2f}% faster")
+        print(f"[Average] This tokenizer is {sum(TestTokenizer.performance) / len(TestTokenizer.performance) * 100:.2f}% faster")
 
         return lib_res, my_res
 
